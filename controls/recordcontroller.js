18893b33cdde75f6d58f3140c5646c42e314815e
Domink Sigmund <sigmund.dominik@googlemail.com>
2018-06-06 11:14:05 +0200

    bugfix records

--- controls/recordcontroller.js
+++ controls/recordcontroller.js
@@ -1,5 +1,6 @@
 var conf = require('../config.json')
 var RECORDS = require('../data/records')
+var moment = require('moment')
 var CronJob = require('cron').CronJob
 const ipc = require('node-ipc')
 const childProcess = require('child_process')
@@ -55,28 +56,34 @@ var job = new CronJob('00 * * * * *', function () { // eslint-disable-line no-un
   if (records.length !== 0) {
     for (var r = 0; r < records.length; r++) {
       var rec = records[r]
-      var commando = ''
-      var timeout = rec.length
-      if (rec.command === 'mplayer') {
-        commando = 'mplayer -dumpstream -dumpfile ' + conf.downloads + '/' + rec.filename + '_id-' + rec.id + '.' + rec.type + ' ' + rec.url
-      } else if (rec.command === 'vlc') {
-        commando = 'vlc ' + rec.url + ' --sout file/' + rec.type + ':' + conf.downloads + '/' + rec.filename + '_id-' + rec.id + '.' + rec.type
-      } else { // streamripper
-        commando = 'streamripper ' + rec.url + ' -a ' + conf.downloads + '/' + rec.filename + '_id-' + rec.id + ' -A --quiet -l ' + rec.length + ' -u winamp'
-        timeout = -1
+
+      var now = moment().tz('Europe/Berlin')
+      var ra = moment(rec.start).tz('Europe/Berlin')
+      var rs = moment(rec.stop).tz('Europe/Berlin')
+      var length = moment.duration(rs.diff(ra)).asSeconds()
+      if (now.isBetween(ra, rs)) {
+        var commando = ''
+        var timeout = length
+        if (rec.command === 'mplayer') {
+          commando = 'mplayer -dumpstream -dumpfile ' + conf.downloads + '/' + rec.filename + '_id-' + rec.id + '.' + rec.type + ' ' + rec.url
+        } else if (rec.command === 'vlc') {
+          commando = 'vlc ' + rec.url + ' --sout file/' + rec.type + ':' + conf.downloads + '/' + rec.filename + '_id-' + rec.id + '.' + rec.type
+        } else { // streamripper
+          commando = 'streamripper ' + rec.url + ' -a ' + conf.downloads + '/' + rec.filename + '_id-' + rec.id + ' -A --quiet -l ' + length + ' -u winamp'
+          timeout = -1
+        }
+
+        console.log("CRON: Executing: '" + commando + "'")
+
+        childProcess.exec(commando, {timeout: timeout}, function () {
+          var m = {}
+          m.type = 'custom'
+          m.text = 'reload'
+          m.sender = rec.id
+        })
       }
-
-      console.log("CRON: Executing: '" + commando + "'")
-
-      childProcess.exec(commando, {timeout: timeout}, function () {
-        var m = {}
-        m.type = 'custom'
-        m.text = 'reload'
-        m.sender = rec.id
-      })
     }
   }
-  // TODO: check if there are records to start
 }, null, true, 'Europe/Berlin')
 
 console.log("'RecordControl' running")
