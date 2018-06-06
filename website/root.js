ef5026c1e05a9c5fb44cbf3e14ea90c2f44336f5
Domink Sigmund <sigmund.dominik@googlemail.com>
2018-06-06 10:47:54 +0200

    diskspace error mgmt

--- website/root.js
+++ website/root.js
@@ -23,50 +23,98 @@ module.exports = function (app, data, functions) {
     switch (req.params.command) {
       case 'free':
         diskspace.check('/', function (err, total2, free2, status2) {
-          return res.send(filesize(free2).toString())
+          if (err) {
+            return res.send('ERR')
+          } else {
+            try {
+              return res.send(filesize(free2).toString())
+            } catch (err) {
+              return res.send('ERR')
+            }
+          }
         })
         break
       case 'pfree':
         diskspace.check('/', function (err, total2, free2, status2) {
-          var percent_free = Math.round((100 * (free2)) / (total2))
-          return res.send(filesize(free2).toString() + ' / ' + percent_free.toString() + '%')
+          if (err) {
+            return res.send('ERR')
+          } else {
+            try {
+              var percentFree = Math.round((100 * (free2)) / (total2))
+              return res.send(filesize(free2).toString() + ' / ' + percentFree.toString() + '%')
+            } catch (err) {
+              return res.send('ERR')
+            }
+          }
         })
         break
       case 'used':
         diskspace.check('/', function (err, total2, free2, status2) {
-          var used = (total2) - (free2)
-          return res.send(filesize(used).toString())
+          if (err) {
+            return res.send('ERR')
+          } else {
+            try {
+              var used = (total2) - (free2)
+              return res.send(filesize(used).toString())
+            } catch (err) {
+              return res.send('ERR')
+            }
+          }
         })
         break
       case 'total':
         diskspace.check('/', function (err, total2, free2, status2) {
-          return res.send(filesize(total2).toString())
+          if (err) {
+            return res.send('ERR')
+          } else {
+            try {
+              return res.send(filesize(total2).toString())
+            } catch (err) {
+              return res.send('ERR')
+            }
+          }
         })
         break
       case 'downloads':
         dir.getSize(app.downloads, function (err, size) {
-          return res.send(filesize(size).toString())
+          if (err) {
+            return res.send('ERR')
+          } else {
+            try {
+              return res.send(filesize(size).toString())
+            } catch (err) {
+              return res.send('ERR')
+            }
+          }
         })
         break
       case 'database':
         dir.getSize(app.database, function (err, size) {
-          return res.send(filesize(size).toString())
+          if (err) {
+            return res.send('ERR')
+          } else {
+            try {
+              return res.send(filesize(size).toString())
+            } catch (err) {
+              return res.send('ERR')
+            }
+          }
         })
         break
       case 'html':
         diskspace.check('/', function (err, total2, free2, status2) {
           dir.getSize(app.downloads, function (err, downloads) {
             dir.getSize(app.database, function (err, database) {
-              var percent_used = Math.round((100 * ((total2) - (free2))) / (total2))
-              var percent_free = 100 - percent_used
+              var percentUsed = Math.round((100 * ((total2) - (free2))) / (total2))
+              var percentFree = 100 - percentUsed
               return res.render('space', {
                 free: filesize(free2),
                 total: filesize(total2),
                 used: filesize(((total2) - (free2))),
                 downloads: filesize(downloads),
                 database: filesize(database),
-                percent_used: percent_used,
-                percent_free: percent_free
+                percentUsed: percentUsed,
+                percentFree: percentFree
               })
             })
           })
