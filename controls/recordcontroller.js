3105784a332faf46c1277c9c103f97f65837be4a
Dominik Sigmund <sigmund.dominik@googlemail.com>
2024-03-12 09:07:12 +0100

    added pid to records

--- controls/recordcontroller.js
+++ controls/recordcontroller.js
@@ -37,6 +37,16 @@ var job = new CronJob('* * * * *', function () { // eslint-disable-line no-unuse
           m.type = 'custom'
           m.text = 'reload'
           m.sender = rec.id
+          childProcess.exec("pgrep -f \"" + commando + "\"", options, function (error, stdout) {
+            rec.streamripper_pid = stdout
+            RECORDS.updateRecord(conf.database, rec, function(error, record) {
+              if (error) {
+                console.error(error)
+              } else {
+                console.log("Record " + record.id + " updated with pid " + record.streamripper_pid)
+              }
+            })
+          })
         })
       }
     }
