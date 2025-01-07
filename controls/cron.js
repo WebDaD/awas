69b1c7d91bab1fca10ed40c76dea1e49af700823
Florian Thoma <flori.thoma@gmail.com>
2025-01-08 00:53:19 +0100

    start the controls of cron jobs on application start

--- controls/cron.js
+++ controls/cron.js
@@ -4,67 +4,69 @@ var CRON = require('cron')
 const childProcess = require('child_process')
 var http = require('http')
 var httpOptions = {
-  host: 'localhost',
-  path: '/crons/reload',
-  method: 'PUT'
+    host: 'localhost',
+    path: '/crons/reload',
+    method: 'PUT'
 }
 
 var cronid = process.argv[2]
 console.log('Worker ' + cronid + ' Starting...')
-var cron = jsonfile.readFileSync(conf.database + '/crons/' + cronid + '.json')
+const cronPath = (conf.database + '/crons/' + cronid + '.json').replace(/\/+/g, '/')
+var cron = jsonfile.readFileSync(cronPath)
+
 var downloads = conf.downloads
 console.log('Adding Cron ' + cronid + ' with tab ' + cron.tab + ' and length ' + cron.length + ' and commando ' + cron.command)
 
 try {
-  var job = new CRON.CronJob({
-    cronTime: cron.tab,
-    onTick: function () {
-      console.log('CRON[' + cronid + ' TICK')
-      var commando = ''
-      var timeout = cron.length
-      if (cron.command === 'mplayer') {
-        commando = 'mplayer -dumpstream -dumpfile ' + downloads + '/' + cron.times_run + '-' + cron.filename + '_id-' + cronid + '.' + cron.type + ' ' + cron.url
-      } else if (cron.command === 'vlc') {
-        commando = 'vlc ' + cron.url + ' --sout file/' + cron.type + ':' + downloads + '/' + cron.times_run + '-' + cron.filename + '_id-' + cronid + '.' + cron.type
-      } else { // streamripper
-        commando = 'streamripper ' + cron.url + ' -a ' + downloads + '/' + cron.times_run + '-' + cron.filename + '_id-' + cronid + ' -A --quiet -l ' + cron.length + ' -u winamp'
-        timeout = -1
-      }
+    var job = new CRON.CronJob({
+        cronTime: cron.tab,
+        onTick: function() {
+            console.log('CRON[' + cronid + ' TICK')
+            var commando = ''
+            var timeout = cron.length
+            if (cron.command === 'mplayer') {
+                commando = 'mplayer -dumpstream -dumpfile ' + downloads + '/' + cron.times_run + '-' + cron.filename + '_id-' + cronid + '.' + cron.type + ' ' + cron.url
+            } else if (cron.command === 'vlc') {
+                commando = 'vlc ' + cron.url + ' --sout file/' + cron.type + ':' + downloads + '/' + cron.times_run + '-' + cron.filename + '_id-' + cronid + '.' + cron.type
+            } else { // streamripper
+                commando = 'streamripper ' + cron.url + ' -a ' + downloads + '/' + cron.times_run + '-' + cron.filename + '_id-' + cronid + ' -A --quiet -l ' + cron.length + ' -u winamp'
+                timeout = -1
+            }
 
-      console.log('CRON[' + cronid + "]: Executing: '" + commando + "'")
+            console.log('CRON[' + cronid + "]: Executing: '" + commando + "'")
 
-      var options = {}
-      if (timeout > 0) {
-        options.timout = timeout
-      }
-      childProcess.exec(commando, options, function () {
-        cron.times_run++
-        jsonfile.writeFileSync(conf.database + '/crons/' + cronid + '.json', cron)
-        console.log('CRON[' + cronid + ']: Done.')
-        writeHTTP()
-      })
-    },
-    start: true,
-    timeZone: 'Europe/Berlin'
-  })
+            var options = {}
+            if (timeout > 0) {
+                options.timout = timeout
+            }
+            childProcess.exec(commando, options, function() {
+                cron.times_run++
+                    jsonfile.writeFileSync(conf.database + '/crons/' + cronid + '.json', cron)
+                console.log('CRON[' + cronid + ']: Done.')
+                writeHTTP()
+            })
+        },
+        start: true,
+        timeZone: 'Europe/Berlin'
+    })
 } catch (ex) {
-  console.log('Worker ' + cronid + ' NOT running: Cron Pattern ' + cron.tab + ' not valid!')
+    console.log('Worker ' + cronid + ' NOT running: Cron Pattern ' + cron.tab + ' not valid!')
 }
 console.log('Worker ' + cronid + ' running: ' + job.running)
 
-function writeHTTP () {
-  var httpReq = http.request(httpOptions, function (res) {
-    var responseString = ''
+function writeHTTP() {
+    var httpReq = http.request(httpOptions, function(res) {
+        var responseString = ''
 
-    res.on('data', function (data) {
-      responseString += data
-        // save all the data from response
+        res.on('data', function(data) {
+            responseString += data
+                // save all the data from response
+        })
+        res.on('end', function() {
+            console.log(responseString)
+                // print to console when response ends
+        })
     })
-    res.on('end', function () {
-      console.log(responseString)
-        // print to console when response ends
-    })
-  })
-  httpReq.write('')
-  httpReq.end()
+    httpReq.write('')
+    httpReq.end()
 }
--- controls/croncontroller.js
+++ controls/croncontroller.js
@@ -2,79 +2,79 @@ var conf = require('../config.json')
 var CRONS = require('../data/crons')
 var CronJob = require('cron').CronJob
 var pm2 = require('pm2')
-const childprogram = '/opt/awas/controls/cron.js'
+const childprogram = '../awas/controls/cron.js'
 
 var crons = CRONS.load(conf.database)
 var childs = {}
 
-pm2.connect(function (err) {
-  if (err) {
-    console.error(err)
-    process.exit(2)
-  } else {
-    for (var x = 0; x < crons.length; x++) {
-      var cron = crons[x]
-      console.log('Starting Worker ' + cron.id)
-      pm2.start({
-        script: childprogram,
-        name: 'CRON_' + cron.id,
-        args: cron.id
-      }, function (err, apps) {
-        if (err) {
-          console.error(err)
-        } else {
-          childs[cron.id] = cron
+pm2.connect(function(err) {
+    if (err) {
+        console.error(err)
+        process.exit(2)
+    } else {
+        for (var x = 0; x < crons.length; x++) {
+            var cron = crons[x]
+            console.log('Starting Worker ' + cron.id)
+            pm2.start({
+                script: childprogram,
+                name: 'CRON_' + cron.id,
+                args: cron.id
+            }, function(err, apps) {
+                if (err) {
+                    console.error(err)
+                } else {
+                    childs[cron.id] = cron
+                }
+            })
         }
-      })
     }
-  }
 })
 
-var job = new CronJob('* * * * *', function () {
-  var oldCrons = crons
-  crons = CRONS.load(conf.database)
-  console.log('CC TICK (' + crons.length + ' Crons)')
-  for (var c = 0; c < crons.length; c++) {
-    var cron = crons[c]
-    for (var oc = 0; oc < oldCrons.length; oc++) {
-      if (oldCrons[oc].id === cron.id && !CRONS.equalCrons(cron, oldCrons[oc])) {
-        console.log('Re-Starting Worker ' + cron.id)
-        pm2.restart('CRON_' + cron.id, function (err) {
-          if (err) {
-            console.error(err)
-          }
-        })
-      }
-    }
-    if (typeof childs[cron.id] === 'undefined') {
-      console.log('Starting Worker ' + cron.id)
-      pm2.start({
-        script: childprogram,
-        name: 'CRON_' + cron.id,
-        args: cron.id
-      }, function (err, apps) {
-        if (err) {
-          console.error(err)
-        } else {
-          childs[cron.id] = cron
+var job = new CronJob('* * * * *', function() {
+    var oldCrons = crons
+    crons = CRONS.load(conf.database)
+    console.log('CC TICK (' + crons.length + ' Crons)')
+    for (var c = 0; c < crons.length; c++) {
+        var cron = crons[c]
+        for (var oc = 0; oc < oldCrons.length; oc++) {
+            if (oldCrons[oc].id === cron.id && !CRONS.equalCrons(cron, oldCrons[oc])) {
+                console.log('Re-Starting Worker ' + cron.id)
+                pm2.restart('CRON_' + cron.id, function(err) {
+                    if (err) {
+                        console.error(err)
+                    }
+                })
+            }
         }
-      })
+        if (typeof childs[cron.id] === 'undefined') {
+            console.log('Starting Worker ' + cron.id)
+            pm2.start({
+                script: childprogram,
+                name: 'CRON_' + cron.id,
+                args: cron.id
+            }, function(err, apps) {
+                if (err) {
+                    console.error(err)
+                } else {
+                    childs[cron.id] = cron
+                }
+            })
+        }
     }
-  }
-  for (var childID in childs) {
-    if (childs.hasOwnProperty(childID)) {
-      if (!CRONS.exists(crons, childID)) {
-        console.log('Stopping Worker ' + childID)
-        pm2.delete('CRON_' + childID, function (err) {
-          if (err) {
-            console.error(err)
-          } else {
-            delete childs[childID]
-          }
-        })
-      }
+    for (var childID in childs) {
+        if (childs.hasOwnProperty(childID)) {
+            if (!CRONS.exists(crons, childID)) {
+                console.log('Stopping Worker ' + childID)
+                pm2.delete('CRON_' + childID, function(err) {
+                    if (err) {
+                        console.error(err)
+                    } else {
+                        delete childs[childID]
+                    }
+                })
+            }
+        }
     }
-  }
 }, null, true, 'Europe/Berlin')
 
 console.log("'CronControl' running: " + job.running)
